9f67b12d90d7203afb60e28bae06fdc3657402fc
odudu-basi <oduduabasiav@gmail.com>
2025-07-25 01:48:11 -0400

    Initial Commit

---
+++ Preppi AI/Preppi_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Preppi_AIApp.swift
+//  Preppi AI
+//
+//  Created by Oduduabasi Victor on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Preppi_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Preppi AITests/Preppi_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Preppi_AITests.swift
+//  Preppi AITests
+//
+//  Created by Oduduabasi Victor on 7/25/25.
+//
+
+import Testing
+@testable import Preppi_AI
+
+struct Preppi_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
